807bb95634a0bf05154a298955ba4806b79e8c8e
Abdulaziz <52410906+mukhamedovabdulaziz@users.noreply.github.com>
2024-03-11 22:24:44 +0500

    upload lab6

---
+++ Lab6/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'albums/:id', component: AlbumDetailComponent },
+  { path: 'albums/:id/photos', component: AlbumPhotosComponent },
+];
