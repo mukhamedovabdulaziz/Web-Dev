5e7db13a4990f90b180ab8a2e2928fe4b3afc2a4
Abdulaziz <52410906+mukhamedovabdulaziz@users.noreply.github.com>
2024-04-07 15:44:34 +0500

    update lab9

--- Lab9/hh_back/api/admin.py
+++ Lab9/hh_back/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Vacancy, Company
 
-# Register your models here.
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'salary', 'company')
+    search_fields = ('name', 'company',)
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'city', 'address')
+    search_fields = ('name', 'city',)
+
+admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Company, CompanyAdmin)
